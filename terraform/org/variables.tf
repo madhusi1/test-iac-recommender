47b196e7b534913006c979444a9aba29f045fd25
rutujapatil-google <rutujapatil@google.com>
2025-06-12 19:41:32 +0530

    Update variables.tf

--- terraform/org/variables.tf
+++ terraform/org/variables.tf
@@ -1,6 +1,19 @@
-variable "project_id" {
-  type        = string
-  description = "The ID of the Google Cloud project"
-  default     = "600587461297"
+variable "organization" {
+  description = "The Google Cloud organization configuration."
+  type = object({
+    id                    = string
+    name                  = string
+    directory_customer_id = string
+    domain                = string
+    billing_account       = optional(string, null)
+  })
+  validation {
+    condition     = can(regex("[0-9]+", var.organization.id))
+    error_message = "ERROR: organization.id must be a number"
+  }
+  validation {
+    condition     = startswith(var.organization.name, "organizations/")
+    error_message = "ERROR: organization.name must be in the format organizations/*"
+  }
 }
 
