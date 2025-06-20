7a6310f60d85c717c2df7d3a925bb39386fe854f
rutujapatil-google <rutujapatil@google.com>
2025-06-20 00:48:34 +0000

    [Gemini Generated] [SCC] Remediation for finding: 9c98edafc24a485bbc5271addbb49cc4, Project: 498208830960, Category UNUSED_IAM_ROLE

--- terraform/org/main.tf
+++ terraform/org/main.tf
@@ -44,3 +44,59 @@ resource "google_project_iam_member" "add_new_role" {
     }
 }
 
+# Resources to remove specific IAM members from project sdw-non-conf-551095-7ae6
+# These resources are added because the specified bindings were not found
+# managed by existing resources in this file.
+resource "google_project_iam_member_remove" "remove_project_iam_admin_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/resourcemanager.projectIamAdmin"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_bigquery_data_viewer_sa_dataflow_reid" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/bigquery.dataViewer"
+  member  = "serviceAccount:sa-dataflow-controller-reid@sdw-conf-551095-7409.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_bigquery_admin_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/bigquery.admin"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_sa_token_creator_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_sa_admin_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/iam.serviceAccountAdmin"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_storage_admin_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_service_usage_admin_dwsalabels" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/serviceusage.serviceUsageAdmin"
+  member  = "serviceAccount:dwsalabels@gfelipesbpsa.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_bigquery_data_editor_sa_dataflow" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:sa-dataflow-controller@sdw-data-ing-551095-4fff.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member_remove" "remove_bigquery_job_user_sa_dataflow" {
+  project = "sdw-non-conf-551095-7ae6"
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:sa-dataflow-controller@sdw-data-ing-551095-4fff.iam.gserviceaccount.com"
+}
